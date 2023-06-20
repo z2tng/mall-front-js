a5e4557b3e71dd2c24d0f849806d1e743751f93d
z2tng <tz_none@163.com>
2023-06-20 10:23:07 +0800

    更换重置密码中使用的分布条

--- src/page/user-password-reset/index.js
+++ src/page/user-password-reset/index.js
@@ -1,31 +1,35 @@
 require("./index.css");
-require("page/user-login/index.css");
 require("page/common/nav-top-simple/index.js")
 
 const _common_util = require("utils/util.js");
 const _user_service = require("service/user-service.js");
 
-const StepHeader = require("utils/step-header/index.js");
+const Process = require("utils/process/index.js");
 
 const errorItem = {
     show: function (errorMsg) {
-        $('.user-form-error').show().find('.error-message').text(errorMsg);
+        $('.form-error').show().find('.error-message').text(errorMsg);
     },
     hide: function () {
-        $('.user-form-error').hide().find('.error-message').text('');
+        $('.form-error').hide().find('.error-message').text('');
     }
 }
 
+// TODO: 更换分步插件
+
 const _user_forget_password_reset = {
     forgetToken: '',    // 保存获取到的token
-    status: false,      // 用于确定是否可以跳转到下一步
-    currentStep: 1,     // 当前所在步骤
-    steps: 3,           // 步骤数量
-    defaultStepData: [
-        {title: '用户验证', tip: '输入用户名'},
-        {title: '回答问题', tip: '回答注册时设置的问题'},
-        {title: '重置密码', tip: '输入新密码'},
-    ],
+    processParam: {
+        container   : null,
+        current     : 1,
+        length      : 3,
+        nodes       : [],
+        titles      : ['用户验证', '回答问题', '重置密码'],
+        descs       : ['输入用户名', '回答注册时设置的问题', '输入新密码'],
+        nodeSize    : 'normal',
+        activeColor : 'blue',
+        lineLength  : '50px',
+    },
     init: function () {
         this.bindEvents();
         this.onLoad();
@@ -45,7 +49,8 @@ const _user_forget_password_reset = {
     },
     next: function () {
         const _this = this;
-        let currentStep = this.currentStep;
+        let processParam = this.processParam;
+        let current = processParam.current;
         let formData = {
             username        : $.trim($('#username').val()),
             question        : $.trim($('#question').val()),
@@ -58,7 +63,7 @@ const _user_forget_password_reset = {
         const validateResult = this.formDataValidate(formData);
         // 根据步骤请求
         if (validateResult.status) {
-            if (currentStep === 1) {
+            if (current === 1) {
                 _user_service.getForgetQuestion(formData.username, function (res) {
                     errorItem.hide();
                     document.getElementById('question').value = res;
@@ -66,7 +71,7 @@ const _user_forget_password_reset = {
                 }, function (errorMsg) {
                     errorItem.show(errorMsg);
                 });
-            } else if (currentStep === 2) {
+            } else if (current === 2) {
                 _user_service.checkForgetQuestion(JSON.stringify(formData, [
                     'username', 'question', 'answer'
                 ]), function (res) {
@@ -76,14 +81,13 @@ const _user_forget_password_reset = {
                 }, function (errorMsg) {
                     errorItem.show(errorMsg);
                 });
-            } else if (currentStep === 3) {
-                console.log(formData);
+            } else if (current === 3) {
                 _user_service.resetForgetPassword(JSON.stringify(formData, [
                     'username', 'newPassword', 'forgetToken'
                 ]), function (res) {
                     errorItem.hide();
                     _common_util.successTips(res);
-                    _common_util.toLogin();
+                    _common_util.toLogin(false);
                 }, function (errorMsg) {
                     errorItem.show(errorMsg);
                 });
@@ -93,37 +97,44 @@ const _user_forget_password_reset = {
         }
     },
     prevStep: function () {
-        let currentStep = this.currentStep;
-        this.currentStep = currentStep > 1 ? currentStep - 1 : 1;
-        this.loadStepHeader(this.currentStep, this.steps, this.defaultStepData);
-        this.loadStepBody(this.currentStep);
+        let processParam = this.processParam;
+        let current = processParam.current;
+        processParam.current = current > 1 ? current - 1 : 1;
+        this.loadProcess(processParam);
+        this.loadForm(processParam.current);
     },
     nextStep: function () {
-        let currentStep = this.currentStep;
-        let steps = this.steps;
-        this.currentStep = currentStep < steps ? currentStep + 1 : currentStep;
-        this.loadStepHeader(this.currentStep, this.steps, this.defaultStepData);
-        this.loadStepBody(this.currentStep);
+        let processParam = this.processParam;
+        let current = processParam.current;
+        let length = processParam.length;
+        processParam.current = current < length ? current + 1 : current;
+
+        this.loadProcess(processParam);
+        this.loadForm(processParam.current);
     },
     formDataValidate: function (formData) {
-        let currentStep = this.currentStep;
+        let processParam = this.processParam;
+        let current = processParam.current;
 
         let result = {
             status  : false,
             msg     : '',
         };
 
-        if (currentStep === 1) {
+        if (current === 1) {
+            // 第一步，验证用户名
             if (!_common_util.validate(formData.username, 'require')) {
                 result.msg = '用户名不能为空';
                 return result;
             }
-        } else if (currentStep === 2) {
+        } else if (current === 2) {
+            // 第二步，验证问题答案
             if (!_common_util.validate(formData.answer, 'require')) {
                 result.msg = '答案不能为空';
                 return result;
             }
-        } else if (currentStep === 3) {
+        } else if (current === 3) {
+            // 第三步，验证密码
             if (!_common_util.validate(formData.newPassword, 'require')) {
                 result.msg = '新密码不能为空';
                 return result;
@@ -142,22 +153,20 @@ const _user_forget_password_reset = {
         return result;
     },
     onLoad: function () {
-        this.loadStepHeader(this.currentStep, this.steps, this.defaultStepData);
-        this.loadStepBody(this.currentStep);
+        this.loadProcess(this.processParam);
+        this.loadForm();
     },
-    // 加载分步主体
-    loadStepBody: function (current) {
-        $('.step-item').hide();
+    // 加载表单内容
+    loadForm: function () {
+        let current = this.processParam.current;
+        $('.step-form-item').hide();
         $('#step-' + current).show();
     },
     // 加载分步插件
-    loadStepHeader: function (current, steps, data) {
-        this.stepHeader ? "" : (this.stepHeader = new StepHeader());
-        this.stepHeader.render({
-            container   : $('.step-header'),
-            current     : current,
-            steps       : steps,
-        }, data);
+    loadProcess: function (userOption) {
+        this.process ? "" : (this.process = new Process());
+        userOption.container ? "" : userOption.container = $('.process-container');
+        this.process.render(userOption);
     },
 };
 
