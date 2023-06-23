201ee43a3f878fd841ce487be22f22822b129dee
z2tng <tz_none@163.com>
2023-06-23 14:30:07 +0800

    实现加减框插件

---
+++ src/utils/input-number/index.js
@@ -0,0 +1,81 @@
+require("./index.css");
+
+const inputNumberTemplate = require("./index.string");
+
+const _common_util = require("utils/util.js");
+
+const InputNumber = function () {
+    this.defaultOption = {
+        container   : null,
+        value       : 1,
+        min         : 1,
+        hasSmaller  : false,
+        max         : 100,
+        hasBigger   : false,
+        size        : 'normal',
+        onChange    : null,
+    };
+};
+
+InputNumber.prototype = {
+    init: function (userOption) {
+        this.render(userOption);
+        this.bindEvent();
+    },
+    render: function (userOption) {
+        // 准备数据
+        this.option = $.extend({}, this.defaultOption, userOption);
+        // 校验
+        if (!(this.option.container instanceof jQuery)) {
+            return;
+        }
+    
+        let option = this.option;
+        option.hasSmaller  = option.value > option.min;
+        option.hasBigger   = option.value < option.max;
+    
+        const inputNumberHTML = _common_util.renderHTML(inputNumberTemplate, {
+            size       : option.size,
+            value      : option.value,
+            hasSmaller : option.hasSmaller,
+            hasBigger  : option.hasBigger,
+        });
+    
+        option.container.html(inputNumberHTML);
+    },
+    bindEvent: function () {
+        // 事件处理
+        // TODO: 不在document上绑定事件，而是在container上绑定事件
+        const _this = this;
+        const container = this.option.container;
+
+        container.on('blur', '.input-number-input', function () {
+            const $this = $(this);
+            typeof _this.option.onChange === 'function' ? _this.option.onChange($this.val()) : null;
+        });
+
+        container.on('click', '.input-number-up', function () {
+            const $this = $(this);
+            if ($this.hasClass('disabled')) {
+                return;
+            }
+
+            const $input = $this.siblings('.input-number-input');
+            $input.attr('value', parseInt($input.val()) + 1);
+            typeof _this.option.onChange === 'function' ? _this.option.onChange($input.val()) : null;
+        });
+
+        container.on('click', '.input-number-down', function () {
+            const $this = $(this);
+            if ($this.hasClass('disabled')) {
+                return;
+            }
+            
+            const $input = $this.siblings('.input-number-input');
+            $input.attr('value', parseInt($input.val()) - 1);
+            typeof _this.option.onChange === 'function' ? _this.option.onChange($input.val()) : null;
+        });
+    }
+};
+
+module.exports = InputNumber;
